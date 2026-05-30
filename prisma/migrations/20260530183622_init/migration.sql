121783fc34b831b42b3c8110e113aecb45e7dcad
Sam-sundar-dhas <samsundardhas2004@gmail.com>
2026-05-31 00:09:27 +0530

    feat: setup prisma and supabase

---
+++ prisma/migrations/20260530183622_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateEnum
+CREATE TYPE "CareerPath" AS ENUM ('CLOUD_ENGINEER', 'DEVOPS_ENGINEER', 'SECURITY_ENGINEER', 'SOLUTIONS_ARCHITECT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "level" INTEGER NOT NULL DEFAULT 1,
+    "xp" INTEGER NOT NULL DEFAULT 0,
+    "careerPath" "CareerPath" NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
